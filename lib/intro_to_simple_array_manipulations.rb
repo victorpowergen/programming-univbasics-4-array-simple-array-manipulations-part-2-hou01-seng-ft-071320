0fe5ea2cfa5e6a922314e1f3deb9b93b6c042564
Adewale Olusegun Ige <victorpowergen@gmail.com>
2020-06-23 04:32:04 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,8 @@
+def using_concat(array, best_array)
+  
+  array = ["raindrops on roses", "whiskers on kittens"]
+  best_array = ["sports cars", "flatiron school"]
+  array.concat(best_array)
+  
+
+end
