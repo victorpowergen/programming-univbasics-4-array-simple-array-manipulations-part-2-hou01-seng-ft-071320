826e73c77568137007dd2e99e073c6462e17f538
Adewale Olusegun Ige <victorpowergen@gmail.com>
2020-06-23 04:58:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,6 +3,6 @@ def using_concat(array, best_array)
   array = ["raindrops on roses", "whiskers on kittens"]
   best_array = ["sports cars", "flatiron school"]
   array.concat(best_array)
-  array = array.concat(best_array)
+  new_array = array.concat(best_array)
 
 end
