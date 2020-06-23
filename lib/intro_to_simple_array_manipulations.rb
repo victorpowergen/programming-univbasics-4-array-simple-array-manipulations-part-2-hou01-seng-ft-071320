83bf01c34cb690331622d8d893b6905effd15e3c
Adewale Olusegun Ige <victorpowergen@gmail.com>
2020-06-23 08:00:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -19,8 +19,7 @@ def using_concat(array, best_array)
  
  def using_flatten(array)
   new_array = []
-  array = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
-  array.each do
+  array = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"].each 
     array.flatten
     end
 
