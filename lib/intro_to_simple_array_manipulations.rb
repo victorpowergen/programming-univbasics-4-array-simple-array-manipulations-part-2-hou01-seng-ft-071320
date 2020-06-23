fbde169312180b8e129c72bc82c529ed1cd3d914
Adewale Olusegun Ige <victorpowergen@gmail.com>
2020-06-23 05:49:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,6 +8,6 @@ def using_concat(array, best_array)
  def using_insert(array, obj)
  array = %w{"Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"}
  obj = "Python"
- new_array = array.insert(obj)
+ array = array.insert(4, obj)
  
  end
