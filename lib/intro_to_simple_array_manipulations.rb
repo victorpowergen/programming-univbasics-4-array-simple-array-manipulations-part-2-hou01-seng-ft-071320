8b2b5e2c1c63da058db029f5fbd263bc9e97e07c
Adewale Olusegun Ige <victorpowergen@gmail.com>
2020-06-23 10:30:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -34,6 +34,6 @@ def using_concat(array, best_array)
 def using_delete_at(array, int)
   array = ["Johnny 5", "R2D2", "Robocop"]
   int = [0, 1, 2]
-  delete_at(int, 2)
+  array.delete_at(int, 2)
 end
    
