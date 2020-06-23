c0dda7586715180ffc5874e5302687b7f64d5a45
Adewale Olusegun Ige <victorpowergen@gmail.com>
2020-06-23 06:07:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,11 +10,12 @@ def using_concat(array, best_array)
  array = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
  obj = "Python"
  array = array.insert(4, obj)
- 
  end
  
  def using_uniq(array)
    array = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
    array.uniq
-   
  end
+ 
+ def using_fatten(array)
+   
