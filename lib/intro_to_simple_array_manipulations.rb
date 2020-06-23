1a2ce9c2e09c1cf2ac70a991dd916b1412e153e9
Adewale Olusegun Ige <victorpowergen@gmail.com>
2020-06-23 08:54:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,8 +25,9 @@ def using_concat(array, best_array)
  
  def using_delete(array, string)
    array = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+   array.uniq
    string = "Steven"
-   array.delete(string).to_set
+   array.delete(string)
   
  end
 
