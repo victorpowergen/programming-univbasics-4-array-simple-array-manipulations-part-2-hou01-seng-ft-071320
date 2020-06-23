8cdeb05f4b558c7319fd2931a0c2e225945cd4f2
Adewale Olusegun Ige <victorpowergen@gmail.com>
2020-06-23 10:36:18 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,39 @@
+def using_concat(array, best_array)
+   array.concat(best_array)
+  array = ["raindrops on roses", "whiskers on kittens"]
+  best_array = ["sports cars", "flatiron school"]
+ array.concat(best_array)
+ end
+ 
+ def using_insert(array, obj)
+   array = array.insert(4, obj)
+ array = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+ obj = "Python"
+ array = array.insert(4, obj)
+ end
+ 
+ def using_uniq(array)
+   array = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
+   array.uniq
+ end
+ 
+ def using_flatten(array)
+   array = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
+   new_array = array.flatten
+   
+ end
+ 
+ def using_delete(array, string)
+   array.delete(string)
+   array = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+   string = "Steven"
+   array.delete(string)
+  
+ end
+ 
+def using_delete_at(array, int)
+  array = ["Johnny 5", "R2D2", "Robocop"]
+  int = [0, 1, 2]
+  array.delete_at(2)
+end
+   
