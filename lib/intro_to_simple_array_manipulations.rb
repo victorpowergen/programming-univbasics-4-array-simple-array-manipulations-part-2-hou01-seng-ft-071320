e778c2797544d015f4b57324ad74c1f7c66c4b67
Adewale Olusegun Ige <victorpowergen@gmail.com>
2020-06-23 06:31:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -19,7 +19,7 @@ def using_concat(array, best_array)
  
  def using_fatten(array)
    array.fatten
-   array = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
-   array.fatten
+   array = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"].fatten
+   puts array
  end
    
